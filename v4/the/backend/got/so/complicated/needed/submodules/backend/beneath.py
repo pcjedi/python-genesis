12e20b2fac445a62e43b7a641793209fbb3534a9
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2023-01-20 16:39:51 +0000

    paths ...

--- v4/the/backend/got/so/complicated/needed/submodules/backend/beneath.py
+++ v4/the/backend/got/so/complicated/needed/submodules/backend/beneath.py
@@ -5,3 +5,7 @@ def makeitrain(duration=1):
     start = time.time()
     while time.time() - start < duration:
         print("Money!!!")
+
+
+def whoami():
+    print("I am ", __name__)
--- v4/the/backend/got/so/complicated/needed/submodules/frontend.py
+++ v4/the/backend/got/so/complicated/needed/submodules/frontend.py
@@ -1,7 +1,11 @@
+import backend
 from backend import complexstuff
 from backend.beneath import makeitrain
 import requests
 
 print(requests.get("http://ip.jsontest.com/").json())
 print(complexstuff)
+backend.beneath.whoami()
 makeitrain()
+backend.beneath.whoami()
+print("my name is: ", __name__)
