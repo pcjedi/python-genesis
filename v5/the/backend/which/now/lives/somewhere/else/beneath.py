898559cc2f4c54e63742c9ef0f6a13a6d35d527c
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2023-01-22 13:12:55 +0000

    less complex for now

--- v5/the/backend/which/now/lives/somewhere/else/beneath.py
+++ v5/the/backend/which/now/lives/somewhere/else/beneath.py
@@ -5,7 +5,9 @@ def makeitrain(duration=1):
     start = time.time()
     while time.time() - start < duration:
         print("Money!!!")
+    return "Money"
 
 
 def whoami():
     print("I am ", __name__)
+    return __name__
